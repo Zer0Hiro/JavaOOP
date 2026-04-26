07702af819e5005824dfd9d4620840d8bb15ec89
Zer0Hiro <chibvk43@gmail.com>
2026-04-26 23:40:16 +0300

    HW2 first question

---
+++ HW/proj2/src/tree/MainForTree.java
@@ -0,0 +1,11 @@
+package tree;
+
+public class MainForTree {
+    public static void main(String[] args) {
+        Node root = new Node();
+        String str = "ello";
+        root.add("hello");;
+        System.out.println("Amount of " + str + " is: " + root.num(str));
+
+    }
+}
---
+++ HW/proj2/src/tree/Node.java
@@ -0,0 +1,40 @@
+package tree;
+
+public class Node {
+    private int count = 0;
+    private Node[] children = new Node[26];
+
+    public int num(String s) {
+        if (s.length() == 0) {
+            return this.count;
+        }
+
+        char letter = s.charAt(0);
+        int index = letter - 'a';
+
+        // Word not in nodes
+        if (children[index] == null)
+            return 0;
+
+        return children[index].num(s.substring(1));
+    }
+
+    public void add(String s) {
+        if (s.length() > 0) {
+            // get first letter
+            char letter = s.charAt(0);
+            // get everything after first letter
+            s = s.substring(1);
+
+            int index = letter - 'a';
+            // create new node if not exist already
+            if (children[index] == null) {
+                children[index] = new Node();
+                children[index].count = 0;
+            }
+            children[index].add(s);
+
+        }
+        count++;
+    }
+}
